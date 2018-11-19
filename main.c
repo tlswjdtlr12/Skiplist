4f9e1fcb799a2016508857605dea6b0235975920
2014004739 <2014004739@hanyang.ac.kr>
2018-11-20 00:14:39 +0900

    insert, find 삽입완료

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 
 typedef struct skiplist * SKlist;
 typedef struct levelist * Levels;
+FILE * close;
 
 typedef struct skiplist {
     uint64_t key;
@@ -121,37 +122,78 @@ void skiplist_free(Levels list)
     free(current_node);
     free(list);
 }
-
-//static void skiplist_dump(Levels list) {
-//    SKlist x = list->header;
-//    while (x && x->forward[1] != list->header) {
-//        printf("%d[%d]->", x->forward[1]->key, x->forward[1]->value);
-//        x = x->forward[1];
-//    }
-//    printf("NIL\n");
-//}
+// 출력법
+static void skiplist_dump(Levels list) {
+    SKlist x = list->header;
+    while (x && x->forward[1] != list->header) {
+        printf("%d[%d]->", x->forward[1]->key, x->forward[1]->value);
+        x = x->forward[1];
+    }
+    printf("NIL\n");
+}
 
 int main() {
-    int arr[] = { 3, 6, 9, 2, 11, 1, 4 }, i;
+    int i;
     Levels list;
+    SKlist F;
     list = (Levels)malloc(sizeof(levelist));
-
     SKlist header = (SKlist) malloc(sizeof(struct skiplist));
     list->header = header;
-
-    // 조낸 큰값이 필요한 알고리즘인듯
-    header->key = 100000;
-
+    header->key = 100000; // 조낸 큰값이 필요한 알고리즘인듯
     header->forward = (SKlist *) malloc(sizeof(SKlist) * (SKIPLIST_MAX_LEVEL + 1));
     for (i = 0; i <= SKIPLIST_MAX_LEVEL; i++) {
         header->forward[i] = list->header;
     }
     list->level = 1;
 
-    printf("Insert:--------------------\n");
-    for (i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) {
-        skiplist_insert(list, arr[i], arr[i]);
+    char infi; // insert, find
+    uint64_t key,val;
+    FILE *open = fopen("input.txt","r");
+    close = fopen("output.txt","w");
+    if(open==NULL){puts("err : file read"); return 0;}
+
+    while(fscanf(open,"%c",&infi) != EOF)
+    {
+        switch(infi) {
+            case 'I':
+                fscanf(open, "%lld%lld", &key, &val);
+                skiplist_insert(list, key, val);
+                skiplist_dump(list);
+//                if(dupli==1){
+//                    fprintf(close,"Found (%d,%d) update v=%d\n",key,pre_value,val);
+//                    dupli=0;
+//                }
+//                else fprintf(close,"Inserted (%d,%d)\n",key,val);
+                break;
+            case 'F':
+                fscanf(open, "%lld", &key);
+                F = skiplist_search(list, key);
+//                if(notfound==1){
+//                    fprintf(close,"Not Found\n");
+//                    notfound=0;
+//                    depth=0;
+//                }
+//                else{
+//                    fprintf(close,"Found (%d,%d) on d=%d with c=",F->Key,F->Value,depth-1); // depth-1 : because header
+//                    if(F->Color==Red)fprintf(close,"red\n");
+//                    else fprintf(close,"black\n");
+//                    depth=0;
+//                }
+                break;
+            case 'Q':
+                // end of file
+                break;
+        }
     }
+
+
+
+
+
+    printf("Insert:--------------------\n");
+//    for (i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) {
+//        skiplist_insert(list, arr[i], arr[i]);
+//    }
    // skiplist_dump(list);
 
     printf("Search:--------------------\n");
@@ -167,74 +209,10 @@ int main() {
     }
 
     printf("Search:--------------------\n");
-    skiplist_delete(list, 3);
-    skiplist_delete(list, 9);
+  //  skiplist_delete(list, 3);
+  //  skiplist_delete(list, 9);
   //  skiplist_dump(list);
     skiplist_free(list);
 
     return 0;
 }
-//
-//int main(int argc, char * argv[]) {
-//    RBTree T, F;
-//
-//    // NN is node with null
-//    if (NN == NULL) {
-//        NN = malloc(sizeof ( struct RedBlackNode));
-//        if (NN == NULL) printf("error");
-//        NN->Left = NN->Right = NN;
-//        NN->Color = Black;
-//    }
-//
-//    // T is header node. T->Right is root
-//    T = malloc(sizeof(struct RedBlackNode));
-//    if (T == NULL) printf("error\n");
-//    T->Left = T->Right = NN;
-//    T->Color = Black;
-//
-//    printf("Inserts are complete\n");
-//    //PrintTree(T->Right); // header skip
-//
-//    char infi; // insert, find
-//    int key,val;
-//    FILE *open = fopen("input.txt","r");
-//    close = fopen("output.txt","w");
-//    if(open==NULL){puts("err : file read"); return 0;}
-//
-//    while(fscanf(open,"%c",&infi) != EOF)
-//    {
-//        switch(infi) {
-//            case 'I':
-//                fscanf(open, "%d%d", &key, &val);
-//                T = Insert(key, val, T);
-//                if(dupli==1){
-//                    fprintf(close,"Found (%d,%d) update v=%d\n",key,pre_value,val);
-//                    dupli=0;
-//                }
-//                else fprintf(close,"Inserted (%d,%d)\n",key,val);
-//                break;
-//            case 'F':
-//                fscanf(open, "%d", &key);
-//                F = Find(key,T);
-//                if(notfound==1){
-//                    fprintf(close,"Not Found\n");
-//                    notfound=0;
-//                    depth=0;
-//                }
-//                else{
-//                    fprintf(close,"Found (%d,%d) on d=%d with c=",F->Key,F->Value,depth-1); // depth-1 : because header
-//                    if(F->Color==Red)fprintf(close,"red\n");
-//                    else fprintf(close,"black\n");
-//                    depth=0;
-//                }
-//                break;
-//            case 'P':
-//                PrintTree(T->Right, close);
-//                fprintf(close, "\n");
-//            case 'Q':
-//                // end of file
-//                break;
-//        }
-//    }
-//    return 0;
-//}

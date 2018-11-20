c8c9126c25b88932cca0553827cce8c2c088e13f
2014004739 <2014004739@hanyang.ac.kr>
2018-11-20 13:42:02 +0900

    finish

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <limits.h>
+#include <time.h>
 
-#define SKIPLIST_MAX_LEVEL 10
+#define MAX_LEVEL 10
+#define BILLION 1000000000ULL
 
 typedef struct skiplist * SKlist;
 typedef struct levelist * Levels;
+struct timespec start, end;
+double elapsed_time;
 FILE * close;
 int dupli, notfound;
 uint64_t pre_value;
@@ -21,85 +25,58 @@ typedef struct levelist {
     struct skiplist * header;
 } levelist;
 
-static int rand_level() {
-    int level = 1;
-    while (rand() < RAND_MAX / 2 && level < SKIPLIST_MAX_LEVEL)
-        level++;
-    return level;
+SKlist level_down(SKlist S, int posit, uint64_t key){
+    while(S->forward[posit]->key < key)
+        S = S->forward[posit];
+    return S;
 }
-
 void Insert(Levels list, uint64_t key, uint64_t value) {
-    SKlist update[SKIPLIST_MAX_LEVEL + 1];
+    int i, level=1;
     SKlist S = list->header;
-    int i, level;
-    for (i = list->level; i >= 1; i--) {
-        while (S->forward[i]->key < key)
-            S = S->forward[i];
-        update[i] = S;
-    }
+    SKlist temp[MAX_LEVEL + 1];
+
+    for (i = list->level; i >= 1; i--) temp[i] = level_down(S, i, key);
     S = S->forward[1];
 
+    // duplicate
     if (key == S->key) {
         pre_value = S->value;
         S->value = value;
         dupli=1;
         return;
     } else {
-        level = rand_level();
+        // random number generator
+        while (2 * rand() < RAND_MAX) {
+            if(level == MAX_LEVEL)break;
+            level++;
+        }
+
         if (level > list->level) {
-            for (i = list->level + 1; i <= level; i++) {
-                update[i] = list->header;
+            for (i = list->level; i < level; i++) {
+                temp[i+1] = list->header;
             }
             list->level = level;
         }
-
         S = (SKlist) malloc(sizeof(skiplist));
         S->key = key;
         S->value = value;
-        S->forward = (SKlist * ) malloc(sizeof(SKlist) * (level + 1));
+        S->forward = (SKlist *)malloc(sizeof(SKlist) * (level + 1));
         for (i = 1; i <= level; i++) {
-            S->forward[i] = update[i]->forward[i];
-            update[i]->forward[i] = S;
+            S->forward[i] = temp[i]->forward[i];
+            temp[i]->forward[i] = S;
         }
     }
 }
 
 SKlist Find(Levels list, int key) {
     SKlist S = list->header;
-    int i;
-    for (i = list->level; i >= 1; i--) {
-        while (S->forward[i]->key < key)
-            S = S->forward[i];
-    }
+    for (int i = list->level; i >= 1; i--) S = level_down(S,i,key);
     if (S->forward[1]->key == key) {
         return S->forward[1];
     } else {
         notfound = 1;
         return NULL;
     }
-}
-
-void skiplist_free(Levels list)
-{
-    SKlist current_node = list->header->forward[1];
-    while(current_node != list->header) {
-        SKlist next_node = current_node->forward[1];
-        free(current_node->forward);
-        free(current_node);
-        current_node = next_node;
-    }
-    free(current_node->forward);
-    free(current_node);
-    free(list);
-}
-// 출력법
-static void skiplist_dump(Levels list) {
-    SKlist S = list->header;
-    while (S && S->forward[1] != list->header) {
-        printf("%d[%d]->", S->forward[1]->key, S->forward[1]->value);
-        S = S->forward[1];
-    }
-    printf("NIL\n");
 }
 
 int main() {
@@ -109,10 +86,11 @@ int main() {
     list = (Levels)malloc(sizeof(levelist));
     SKlist H = (SKlist) malloc(sizeof(struct skiplist)); // H is Header
 
+    // header setting
     list->header = H;
-    H->key = ULLONG_MAX; // 조낸 큰값이 필요한 알고리즘인듯
-    H->forward = (SKlist *) malloc(sizeof(SKlist) * (SKIPLIST_MAX_LEVEL + 1));
-    for (i = 0; i <= SKIPLIST_MAX_LEVEL; i++) {
+    H->key = ULLONG_MAX;
+    H->forward = (SKlist *) malloc(sizeof(SKlist) * (MAX_LEVEL + 1));
+    for (i = 0; i <= MAX_LEVEL; i++) {
         H->forward[i] = list->header;
     }
     list->level = 1;
@@ -123,13 +101,14 @@ int main() {
     close = fopen("output.txt","w");
     if(open==NULL){puts("err : file read"); return 0;}
 
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
     while(fscanf(open,"%c",&infi) != EOF)
     {
         switch(infi) {
             case 'I':
                 fscanf(open, "%lld%lld", &key, &val);
                 Insert(list, key, val);
-                //skiplist_dump(list);
                 if(dupli==1){
                     fprintf(close,"Found (%lld,%lld) update v=%lld\n",key,pre_value,val);
                     dupli=0;
@@ -152,9 +131,23 @@ int main() {
                 break;
         }
     }
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    elapsed_time = (end.tv_nsec - start.tv_nsec) + (end.tv_sec - start.tv_sec) * BILLION;
+    printf("elapsed_time : %f", elapsed_time);
 
-  //  skiplist_dump(list);
-    skiplist_free(list);
+    // list free
+    SKlist current = list->header->forward[1];
+    SKlist next;
+    while(1) {
+        if(current == list->header)break;
+        next = current->forward[1];
+        free(current->forward);
+        free(current);
+        current = next;
+    }
+    free(current->forward);
+    free(current);
+    free(list);
 
     return 0;
 }
